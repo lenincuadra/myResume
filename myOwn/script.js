dab3f61c470071d0a95fb81f258dc3ad7d0ca9c3
Lenin <leningraphic@gmail.com>
2024-10-12 00:03:43 -0300

    Initial Commit

---
+++ myOwn/script.js
@@ -0,0 +1,22 @@
+function displayInfo (){
+    const el = document.getElementsByTagName("li");
+    const p = document.querySelector("p");
+    p.innerText = el.length;
+    }
+
+
+// Dolares:
+const dolarMep = 1185;
+const dolarCrypto = 1201;
+const dolarApp = (dolarMep+dolarCrypto)/2;
+
+
+// Archived to the future product DolarApp 
+
+// let button1 = "Come on!!!"
+
+// let dolarMep = 1200;
+// let dolarCrypto = 1210;
+
+// const dolarApp = (dolarCrypto+dolarMep)/2;
+// console.log(dolarApp);
