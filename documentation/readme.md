290c9953142ab9a8b2f78a11ba5d867bdb52c204
Lenin <leningraphic@gmail.com>
2024-11-18 20:01:54 -0300

    First Product Delivered EVER

---
+++ documentation/readme.md
@@ -0,0 +1,27 @@
+# Perfect Match CV!
+
+## Why this CV is valuable?
+
+I've always been fightging against having:
+
+- A well written CV
+- well designed
+- readable & Scannable for Recruiters and Tech people
+- AND Passing the Parsing for ATS
+
+Even if I'd have it, it's pretty hard to keep up to day, updating just 1 work takes too much times and most of the time I have to do it twice, in English and Spanish, so every change is challenging itself and the real value of a CV is fiding interviews, it has nothing to do with anything else listed before, That why I built this for myself, but expecting to make it scalable to share it to everyone!
+
+---
+
+### How this product works.
+
+1. Edit what you need on your CV
+2. Hit Options / Print on your browser (Chrome Recommended)
+3. You'll need custom size to fix all your CV content inside 1 page and don't break it down to more than 1 page.
+4. After measure the viewport of your content on pixels you have to make the conversion to mm (milimiters) (px-to-mm)[https://pixelsconverter.com/pixels-to-inches]
+5. Add the custom size on the [Print System Dialog](image-1.png!)
+6. Save and Enjoy your CV!
+
+# 😊
+
+# :smile:
--- exportPDF.js
+++ exportPDF.js
@@ -1,32 +1,33 @@
-function exportPDF() {
-  const element = document.getElementById("downloadcv");
-  const options = {
-    margin: 1,
-    filename: "document.pdf",
-    html2canvas: {
-      scale: 2, // Mayor escala mejora el texto y lo hace seleccionable
-      useCORS: true, // Asegura la carga correcta de recursos externos
-    },
-    jsPDF: {
-      unit: "pt",
-      format: [1920, 2000],
-      orientation: "portrait",
-    },
-  };
-  html2pdf().set(options).from(document.getElementById("downloadcv")).save();
-}
 // function exportPDF() {
 //   const element = document.getElementById("downloadcv");
 //   const options = {
-//     margin: [1, 0, 0, 0],
-
-//     filename: "exported-card.pdf",
-
-//     image: { type: "pdf", quality: 0.98 },
-
-//     html2canvas: { scale: 1 },
-//     jsPDF: { unit: "px", format: [1920, 2000], orientation: "portrait" },
-//     // jsPDF: { unit: "pt", format: "a4", orientation: "portrait" },
+//     margin: 1,
+//     filename: "document.pdf",
+//     html2canvas: {
+//       scale: 2, // Mayor escala mejora el texto y lo hace seleccionable
+//       useCORS: true, // Asegura la carga correcta de recursos externos
+//     },
+//     jsPDF: {
+//       unit: "pt",
+//       format: [1920, 2000],
+//       orientation: "portrait",
+//     },
 //   };
-//   html2pdf().set(options).from(element).save();
+//   console.log("HOlanda!");
+//   html2pdf().set(options).from(document.getElementById("downloadcv")).save();
 // }
+function exportPDF() {
+  const element = document.getElementById("downloadcv");
+  const options = {
+    margin: [1, 0, 0, 0],
+
+    filename: "exported-card.pdf",
+
+    image: { type: "pdf", quality: 1 },
+
+    html2canvas: { scale: 1 },
+    jsPDF: { unit: "px", format: [1920, 2000], orientation: "portrait" },
+    // jsPDF: { unit: "pt", format: "a4", orientation: "portrait" },
+  };
+  html2pdf().set(options).from(element).save();
+}
